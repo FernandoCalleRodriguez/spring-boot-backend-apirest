fa39eaad0e094ab12250c9094f5cebc1b50ddd3c
FernandoCalleRodriguez <60648315+FernandoCalleRodriguez@users.noreply.github.com>
2021-07-30 10:54:16 +0200

    Commit inicial
    Commit inicial

---
+++ src/main/java/com/bolsadeideas/spring/boot/backend/apirest/controllers/ClienteRestController.java
@@ -0,0 +1,290 @@
+package com.bolsadeideas.spring.boot.backend.apirest.controllers;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.bolsadeideas.spring.boot.backend.apirest.models.emtity.Cliente;
+import com.bolsadeideas.spring.boot.backend.apirest.models.services.IClienteService;
+
+@CrossOrigin(origins= {"http://localhost:4200"})
+@RestController
+@RequestMapping("/api")
+public class ClienteRestController {
+
+	@Autowired
+	private IClienteService clienteService;
+	
+	private final Logger log = LoggerFactory.getLogger(ClienteRestController.class);
+	
+	@GetMapping("/clientes")
+	public List<Cliente> index(){
+		return clienteService.findAll();
+	}
+	
+	@GetMapping("/clientes/page/{page}")
+	public Page<Cliente> index(@PathVariable Integer page){
+		return clienteService.findAll(PageRequest.of(page, 4));
+	}
+	
+	@GetMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<?> show(@PathVariable Long id){
+		Cliente cliente = null;
+		Map<String, Object> response = new HashMap();
+
+		try{
+			cliente = clienteService.findById(id);
+
+		}catch(NoSuchElementException e) {
+			response.put("mensaje", "Error al realizar la consulta en la base de datos");
+			response.put("error", e.getMessage());
+
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		if (cliente == null) {
+			response.put("mensaje", "El cliente ID:".concat(id.toString().concat(" no existe en la base de datos!")));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
+	}
+	
+	@PostMapping("/clientes")
+	public ResponseEntity<Map<String, Object>> create(@Valid @RequestBody Cliente cliente, BindingResult result){
+		
+		Cliente clienteCreated = null;
+		Map<String, Object> response = new HashMap();
+		
+		if(result.hasErrors()) {
+			
+			/*List<String> errors = new ArrayList<>();
+			for(FieldError err: result.getFieldErrors()) {
+				errors.add("El campo '" + err.getField() + "' " + err.getDefaultMessage());
+			}*/
+			
+			List<String> errors = result.getFieldErrors().stream()
+					.map(err -> "El campo '" + err.getField() + "' " + err.getDefaultMessage())
+					.collect(Collectors.toList());
+			
+			response.put("error", errors);
+
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+		}
+		
+		try{
+
+			clienteCreated = clienteService.save(cliente);
+
+		}catch(DataAccessException e) {
+			response.put("mensaje", "Error al realizar la consulta en la base de datos");
+			response.put("error", e.getMessage().concat(": ".concat(e.getMostSpecificCause().toString())));
+
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		response.put("mensaje","El cliente se ha insertado con exito");
+		response.put("cliente", clienteCreated);
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/clientes/{id}")
+	public ResponseEntity<?> update(@Valid @RequestBody Cliente cliente, BindingResult result, @PathVariable Long id) {
+		Map<String, Object> response = new HashMap();
+		Cliente clienteModificado = null;
+		Cliente clienteActual = null;
+		if(result.hasErrors()) {
+			
+			List<String> errors = result.getFieldErrors().stream()
+					.map(err -> "El campo '" + err.getField() + "' " + err.getDefaultMessage())
+					.collect(Collectors.toList());
+			
+			response.put("error", errors);
+
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+		}
+
+		try{
+			clienteActual = clienteService.findById(id);
+
+		}catch(NoSuchElementException e) {
+			response.put("mensaje", "Error al realizar la consulta en la base de datos");
+			response.put("error", e.getMessage());
+
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		if (clienteActual == null) {
+			response.put("mensaje", "Error, no se puede editar, el cliente ID:".concat(id.toString().concat(" no existe en la base de datos!")));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+		
+
+		try{
+			clienteActual.setApellido(cliente.getApellido());
+			clienteActual.setNombre(cliente.getNombre());
+			clienteActual.setEmail(cliente.getEmail());
+			clienteActual.setCreateAt(cliente.getCreateAt());
+
+		clienteModificado = clienteService.save(clienteActual);
+		}catch(DataAccessException e) {
+			response.put("mensaje", "Error al realizar la consulta en la base de datos");
+			response.put("error", e.getMessage().concat(": ".concat(e.getMostSpecificCause().toString())));
+
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		response.put("mensaje","El cliente se ha insertado con exito");
+		response.put("cliente", clienteModificado);
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		Map<String, Object> response = new HashMap();
+		
+		Cliente clienteActual = clienteService.findById(id);
+		String nombreFotoAnterior =clienteActual.getFoto();
+		
+		if(nombreFotoAnterior != null && nombreFotoAnterior.length() > 0) {
+			Path rutaFotoAnterior = Paths.get("uploads").resolve(nombreFotoAnterior).toAbsolutePath();
+			File archivoAnterior = rutaFotoAnterior.toFile();
+			if(archivoAnterior.exists() && archivoAnterior.canRead()) {
+				archivoAnterior.delete();
+			}
+			
+		}
+		
+		try{
+			clienteService.delete(id);
+
+		}catch(DataAccessException e) {
+			response.put("mensaje", "Error al eliminar el cliente de la base de datos");
+			response.put("error", e.getMessage().concat(": ".concat(e.getMostSpecificCause().toString())));
+
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		response.put("mensaje", "El cliente ha sido eliminado con exito.");
+
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value ="/clientes/upload",
+	        consumes = { "multipart/form-data" },
+	        method = RequestMethod.POST)
+	public ResponseEntity<?> upload(@RequestParam("archivo") MultipartFile archivo, @RequestParam("id") Long id){
+		Map<String, Object> response = new HashMap();
+		System.out.print("entro");
+
+		Cliente clienteActual = null;
+		try{
+			clienteActual = clienteService.findById(id);
+
+		}catch(NoSuchElementException e) {
+			response.put("mensaje", "Error al realizar la consulta en la base de datos");
+			response.put("error", e.getMessage());
+
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		if(!archivo.isEmpty()) {
+			String nombreArchivo = UUID.randomUUID().toString() +"_"+ archivo.getOriginalFilename().replace(" ", "");
+			Path rutaArchivo = Paths.get("uploads").resolve(nombreArchivo).toAbsolutePath();
+			log.info(rutaArchivo.toString());
+			try {
+				System.out.print("llego");
+				Files.copy(archivo.getInputStream(), rutaArchivo);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				response.put("mensaje", "Error al subir la imagen del cliente ".concat(nombreArchivo));
+				response.put("error", e.getMessage().concat(": ").concat(e.getCause().getMessage()));
+				return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+			
+			String nombreFotoAnterior =clienteActual.getFoto();
+			
+			if(nombreFotoAnterior != null && nombreFotoAnterior.length() > 0) {
+				Path rutaFotoAnterior = Paths.get("uploads").resolve(nombreFotoAnterior).toAbsolutePath();
+				File archivoAnterior = rutaFotoAnterior.toFile();
+				if(archivoAnterior.exists() && archivoAnterior.canRead()) {
+					archivoAnterior.delete();
+				}
+				
+			}
+			
+			clienteActual.setFoto(nombreArchivo);
+			clienteService.save(clienteActual);
+			
+			response.put("cliente", clienteActual);
+			response.put("mensaje", "Has subid correctamente la imagen: " + nombreArchivo);
+
+		}
+		
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
+
+	}
+	
+	@GetMapping("/uploads/img/{nombreFoto:.+}")
+	public ResponseEntity<Resource> verFoto(@PathVariable String nombreFoto){
+		
+		Path rutaArchivo = Paths.get("uploads").resolve(nombreFoto).toAbsolutePath();
+		log.info(rutaArchivo.toString());
+
+		Resource recurso = null;
+		try {
+			recurso = new UrlResource(rutaArchivo.toUri());
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		}
+
+		if(!recurso.exists() || !recurso.isReadable()) {
+			throw new RuntimeException("Error no se pudo cargar la imagen: " + nombreFoto);
+		}
+		
+		HttpHeaders cabecera = new HttpHeaders();
+		cabecera.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+ recurso.getFilename()+"\"");
+		return new ResponseEntity<Resource>(recurso,cabecera, HttpStatus.OK);
+
+	}
+	
+}
---
+++ src/main/java/com/bolsadeideas/spring/boot/backend/apirest/models/dao/IClienteDao.java
@@ -0,0 +1,8 @@
+package com.bolsadeideas.spring.boot.backend.apirest.models.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.bolsadeideas.spring.boot.backend.apirest.models.emtity.Cliente;
+
+public interface IClienteDao extends JpaRepository<Cliente, Long>{
+
+}
